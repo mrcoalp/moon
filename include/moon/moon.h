7b214e8872da5d0f79283ebbd86870063cfcd4d1
Marco Pinto <marco.alp120@gmail.com>
2021-03-06 18:52:08 +0000

    Added prefix to logs

--- include/moon/moon.h
+++ include/moon/moon.h
@@ -192,17 +192,22 @@ public:
 
     /// Info log.
     /// \param message Message to log.
-    static void Info(const std::string& message) { s_callback(Level::Info, message); }
+    static void Info(const std::string& message) { log(Level::Info, message); }
 
     /// Warning log.
     /// \param message Message to log.
-    static void Warning(const std::string& message) { s_callback(Level::Warning, message); }
+    static void Warning(const std::string& message) { log(Level::Warning, message); }
 
     /// Error log.
     /// \param message Message to log.
-    static void Error(const std::string& message) { s_callback(Level::Error, message); }
+    static void Error(const std::string& message) { log(Level::Error, message); }
 
 private:
+    /// Logs a new message to callback.
+    /// \param level Level of log.
+    /// \param message Message to log.
+    static void log(Level level, const std::string& message) { s_callback(level, std::string("Moon :: ").append(message)); }
+
     /// Called when a log occurs, receiving log severity and message.
     static inline std::function<void(Level, const std::string&)> s_callback{};
 };
